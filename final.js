34f199bf25863e26c6365b6c3df52edeb12bfd81
Ibne-Shahid <anasbinshahid2004@gmail.com>
2025-08-14 00:27:48 +0600

    Problem-05 solved

--- final.js
+++ final.js
@@ -59,3 +59,44 @@ function bestTeam(player1, player2) {
         
 
 }
+
+
+// Function for Problem-05
+
+function resultReport(marks) {
+
+    if (Array.isArray(marks) !== true) {
+        return "Invalid"
+    } else if (marks.length === 0) {
+        return {
+            finalScore: 0,
+            pass: 0,
+            fail: 0
+        }
+    }
+
+    let total = 0;
+    let passedSubs = 0;
+    let failedSubs = 0;
+
+    for (let i = 0; i < marks.length; i++) {
+        total = total + marks[i]
+
+        if (marks[i] >= 40) {
+            passedSubs = passedSubs + 1
+        } else {
+            failedSubs = failedSubs + 1
+        }
+
+    }
+
+    let averageMark = Math.round(total / marks.length)
+
+    const resultReport = {
+        finalScore: averageMark,
+        pass: passedSubs,
+        fail: failedSubs
+    }
+
+    return resultReport
+}
---
+++ problem-04.js
@@ -0,0 +1,9 @@
+// Function for Problem-04
+
+function  isSame(arr1 , arr2 ) {
+
+    
+
+}
+
+isSame([1, 2, 3] , [1 , 2 , 3])
