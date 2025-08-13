ec0660aab1ff4fee96da435a719c7fb31b80f66f
Ibne-Shahid <anasbinshahid2004@gmail.com>
2025-08-14 00:54:09 +0600

    Assignment complete

--- final.js
+++ final.js
@@ -61,6 +61,33 @@ function bestTeam(player1, player2) {
 }
 
 
+// Function for Problem-04
+
+function  isSame(arr1 , arr2 ) {
+
+    if (Array.isArray(arr1) !== true || Array.isArray(arr2)!== true){
+        return "Invalid"
+    }
+
+    if (arr1.length !== arr2.length){
+
+        return false
+
+    };
+
+    for (let i=0; i<arr1.length; i++){
+        if (arr1[i] !== arr2[i]){
+            return false
+        }
+    }
+
+    return true
+
+
+
+}
+
+
 // Function for Problem-05
 
 function resultReport(marks) {
--- problem-04.js
+++ problem-04.js
@@ -2,8 +2,24 @@
 
 function  isSame(arr1 , arr2 ) {
 
-    
+    if (Array.isArray(arr1) !== true || Array.isArray(arr2)!== true){
+        return "Invalid"
+    }
+
+    if (arr1.length !== arr2.length){
+
+        return false
+
+    };
+
+    for (let i=0; i<arr1.length; i++){
+        if (arr1[i] !== arr2[i]){
+            return false
+        }
+    }
+
+    return true
+
+
 
 }
-
-isSame([1, 2, 3] , [1 , 2 , 3])
